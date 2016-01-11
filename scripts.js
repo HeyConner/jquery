594bd7bdcd03773934b4e4665c3e094b17975780
Conner Belvin and timothea mcguire <c9er99@gmail.com>
2016-01-11 13:55:19 -0800

    jQuery stuff

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $("h1").click(function() {
+  alert("This is a header.");
+  alert("I told you this was a header.")
+});
+$("h2").click(function() {
+  alert("This is a smaller header.");
+});
+
+$("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+$("img").click(function() {
+  alert("This is an image.");
+  });
+
+$("li").dblclick(function() {
+  alert("This is a true list.");
+});
+});
